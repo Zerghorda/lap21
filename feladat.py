eecb743c71055fd2fff3b4c199caee5985e3346d
tar.gergo <tar.gergo21@gmail.com>
2023-02-01 13:29:26 +0100

    tesztek

--- feladat.py
+++ feladat.py
@@ -19,9 +19,9 @@ def jatekos_vesztett_teszt():
     kapott: str = eredmeny(jatekosP, gepP)
     vart: str = "Játékos veszített"
     if kapott == vart:
-        print("A teszteset sikeres!")
+        print("A teszteset jó!")
     else:
-        print("A teszteset sikertelen!")
+        print("A teszteset nem jó!")
 
 def gep_vesztett_teszt():
     jatekosP: list[int] = [7, 8, 10]
@@ -30,20 +30,20 @@ def gep_vesztett_teszt():
     kapott: str = eredmeny(jatekosP, gepP)
     vart: str = "Gép veszített"
     if kapott == vart:
-        print("A teszteset sikeres!")
+        print("A teszteset jó!")
     else:
-        print("A teszteset sikertelen!")
+        print("A teszteset nem jó!")
 
 def dontetlen():
-    jatekosP: list[int] = [8, 10]
+    jatekosP: list[int] = [10, 8]
     gepP: list[int] = [10, 8]
 
     kapott: str = eredmeny(jatekosP, gepP)
     vart: str = "Döntetlen"
     if kapott == vart:
-        print("A teszteset sikeres!")
+        print("A teszteset jó!")
     else:
-        print("A teszteset sikertelen!")
+        print("A teszteset nem jó!")
 
 
 def tesztek():
