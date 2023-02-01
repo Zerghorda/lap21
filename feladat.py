931feca63f066ade228120a518b432021c390276
tar.gergo <tar.gergo21@gmail.com>
2023-02-01 12:38:46 +0100

    lista appendel+ random.randint

--- feladat.py
+++ feladat.py
@@ -1,8 +1,18 @@
 # megoldas
-def eredmeny():
-    pass
-# teszt
+import random
+def eredmeny(jatekosP, gepP):
+    if jatekosP > 21:
+        print("játékos vesztett")
+    if gepP > 21:
+        print("gép vesztett")
+    # teszt
 
+
+
+jatekosP = []
+gepP = [6, 8, 4, 5]
+jatekosP.append(random.randint(1, 30))
+gepP.append(random.randint(1, 30))
 
 def teszt():
-    pass
+ pass
---
+++ main.py
@@ -0,0 +1,2 @@
+import feladat
+feladat.eredmeny()
