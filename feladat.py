bca4bdfe9dbe86042321be1fa02eb2d61cd29abc
tar.gergo <tar.gergo21@gmail.com>
2023-02-02 13:16:44 +0100

    több teszt jatékos vesztéséhez,gép vesztés,döntetlen

--- feladat.py
+++ feladat.py
@@ -15,35 +15,103 @@ def pontokOsszeg(lista: list[int]) -> int:
 def jatekos_vesztett_teszt():
     jatekosP: list[int] = [7, 8, 10]
     gepP: list[int] = [10, 9, 2]
-    jatekosP < gepP
-    jatekosP > 21
     if jatekosP == gepP:
-        jatekosP[list] > gepP[list]
-
-
-
+       jatekosP > gepP
     kapott: str = eredmeny(jatekosP, gepP)
     vart: str = "Játékos veszített"
     if kapott == vart:
         print("A teszteset jó!")
     else:
         print("A teszteset nem jó!")
+def jatekos_veszitet_tullepel():
+    jatekosP: list[int] = [10, 12]
+    if jatekosP > 21:
+        kapott: str = eredmeny(jatekosP)
+        vart: str = "Játékos veszített"
+        if kapott == vart:
+            print("A teszteset jó!")
+        else:
+            print("A teszteset nem jó!")
 
+
+def jatekos_veszitet_dontetlennel():
+    jatekosP: list[int] = [5, 5, 6]
+    gepP: list[int] = [8, 8]
+    print("játékos veszített döntetlennél")
+    vart: str = "döntetlen"
+    kapott: str = eredmeny(jatekosP, gepP)
+    if vart == kapott and len(jatekosP) >len(gepP):
+        if kapott == vart:
+            print("A teszteset jó!")
+        else:
+            print("A teszteset nem jó!")
+
+def jatekos_vesztet_kozelebb():
+    jatekosP: list[int] = [5, 5, 6]
+    gepP: list[int] = [8, 8]
+    print("játékos veszítt mert messzebb volt")
+    if jatekosP > 21 or jatekosP < gepP:
+        kapott: str = eredmeny(jatekosP)
+        vart: str = "Játékos veszített"
+        if kapott == vart:
+            print("A teszteset jó!")
+        else:
+            print("A teszteset nem jó!")
+
+def gep_vesztet_kozelebb():
+    jatekosP: list[int] = [8, 8]
+    gepP: list[int] = [5, 5, 6]
+    print("játékos veszítt mert messzebb volt")
+    if gepP > 21 or jatekosP > gepP:
+        kapott: str = eredmeny(gepP)
+        vart: str = "Gép veszített"
+        if kapott == vart:
+            print("A teszteset jó!")
+        else:
+            print("A teszteset nem jó!")
 def gep_vesztett_teszt():
     jatekosP: list[int] = [7, 8, 10]
     gepP: list[int] = [10, 9, 2]
-
     kapott: str = eredmeny(jatekosP, gepP)
     vart: str = "Gép veszített"
     if kapott == vart:
         print("A teszteset jó!")
     else:
         print("A teszteset nem jó!")
+def gep_veszitet_dontetlennel():
+    jatekosP: list[int] = [8, 8]
+    gepP: list[int] = [5, 5, 6]
+    print("Gép veszített döntetlennél")
+    vart: str = "döntetlen"
+    kapott: str = eredmeny(jatekosP, gepP)
+    if vart == kapott and len(jatekosP) <len(gepP):
+        if kapott == vart:
+            print("A teszteset jó!")
+        else:
+            print("A teszteset nem jó!")
+def gep_veszitet_tullepel():
+    gepP: list[int] = [10, 12]
+    if gepP > 21:
+        kapott: str = eredmeny(gepP)
+        vart: str = "Játékos veszített"
+        if kapott == vart:
+            print("A teszteset jó!")
+        else:
+            print("A teszteset nem jó!")
 
-def dontetlen():
+def dontetlen_teszt():
     jatekosP: list[int] = [10, 8]
     gepP: list[int] = [10, 8]
-
+    kapott: str = eredmeny(jatekosP, gepP)
+    vart: str = "Döntetlen"
+    if kapott == vart:
+        print("A teszteset jó!")
+    else:
+        print("A teszteset nem jó!")
+def dontettlen_teszt_mindketto_veszit():
+    jatekosP: list[int] = [10, 12]
+    gepP: list[int] = [10, 12]
+    print("Döntetlen")
     kapott: str = eredmeny(jatekosP, gepP)
     vart: str = "Döntetlen"
     if kapott == vart:
@@ -51,9 +119,14 @@ def dontetlen():
     else:
         print("A teszteset nem jó!")
 
-
 def tesztek():
     jatekos_vesztett_teszt()
+    jatekos_veszitet_tullepel()
+    jatekos_veszitet_dontetlennel()
+    jatekos_vesztet_kozelebb()
+    gep_vesztet_kozelebb()
     gep_vesztett_teszt()
-    dontetlen()
+    gep_veszitet_dontetlennel()
+    gep_veszitet_tullepel()
+    dontetlen_teszt()
 tesztek()
